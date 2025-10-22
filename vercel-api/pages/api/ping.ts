3070a0f6ec6d7f9511872ee0af46a886d9ecef0e
daichinakamura1030-svg <daichi.nakamura.1030@gmail.com>
2025-10-22 21:28:26 +0900

    Update ping.ts

--- vercel-api/pages/api/ping.ts
+++ vercel-api/pages/api/ping.ts
@@ -1,4 +1,4 @@
-import type { NextApiRequest, NextApiResponse } from "next";
+import { NextApiRequest, NextApiResponse } from "next";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   res.status(200).json({ ok: true, ts: Date.now() });
