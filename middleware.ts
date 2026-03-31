e19732b95f9ef677a71a11003160008da9664916
daichinakamura1030-svg <daichi.nakamura.1030@gmail.com>
2026-03-31 16:10:17 +0900

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -5,15 +5,16 @@ import { guestRegex, isDevelopmentEnvironment } from "./lib/constants";
 export async function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
 
-  /*
-   * Playwright starts the dev server and requires a 200 status to
-   * begin the tests, so this ensures that the tests can start
-   */
   if (pathname.startsWith("/ping")) {
     return new Response("pong", { status: 200 });
   }
 
   if (pathname.startsWith("/api/auth")) {
+    return NextResponse.next();
+  }
+
+  // ここを追加
+  if (pathname.startsWith("/api/line/")) {
     return NextResponse.next();
   }
 
@@ -47,13 +48,6 @@ export const config = {
     "/api/:path*",
     "/login",
     "/register",
-
-    /*
-     * Match all request paths except for the ones starting with:
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
-     */
     "/((?!_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
   ],
 };
